69b74bfe401e55a043f5ecd42da85d36672765ae
Paul Brubaker <pbrubak@gmail.com>
2021-12-05 22:20:20 -0800

    try to fix cors

--- src/services/fetchByProxy.js
+++ src/services/fetchByProxy.js
@@ -1,7 +1,6 @@
 export default async function fetchByProxy(url, params) {
   const response = await fetch('https://proxy-fetch.herokuapp.com/', {
     method: 'POST',
-    mode: 'cors',
     body: {
       method: 'GET',
       url,
