4e5f99dcc872a7007933730c83cb6c5db860bcc5
Paul Brubaker <pbrubak@gmail.com>
2021-12-05 22:35:28 -0800

    Die CORS, die!

--- src/services/fetchByProxy.js
+++ src/services/fetchByProxy.js
@@ -1,7 +1,9 @@
 export default async function fetchByProxy(url, params) {
   const response = await fetch('https://proxy-fetch.herokuapp.com/', {
     method: 'POST',
-    'Content-Type': 'application/json',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: {
       method: 'GET',
       url,
