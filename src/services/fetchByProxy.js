5236f52bc306a4b79bf2c72a1e9112d59fcec290
Paul Brubaker <pbrubak@gmail.com>
2021-12-05 22:31:08 -0800

    I still dont know why cors wont work

--- src/services/fetchByProxy.js
+++ src/services/fetchByProxy.js
@@ -1,6 +1,7 @@
 export default async function fetchByProxy(url, params) {
   const response = await fetch('https://proxy-fetch.herokuapp.com/', {
     method: 'POST',
+    'Content-Type': 'application/json',
     body: {
       method: 'GET',
       url,
